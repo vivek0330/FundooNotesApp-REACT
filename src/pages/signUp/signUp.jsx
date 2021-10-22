ca28fb85af06fcc837dbec0d73ea6d5b45678a8e
vivek <vivek.iitd93@gmail.com>
2021-10-22 22:18:41 +0530

    Refactor: changes in design for sign up form

--- src/pages/signUp/signUp.jsx
+++ src/pages/signUp/signUp.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { Grid, Paper, TextField, Button, Typography } from "@material-ui/core";
-import Radio from "@material-ui/core/Radio";
-import RadioGroup from "@material-ui/core/RadioGroup";
+
 import FormControlLabel from "@material-ui/core/FormControlLabel";
-import FormControl from "@material-ui/core/FormControl";
-import FormLabel from "@material-ui/core/FormLabel";
 import "./sinUp.scss";
 import * as Yup from "yup";
 import Checkbox from "@material-ui/core/Checkbox";
@@ -48,7 +45,7 @@ const SignUp = () => {
       .typeError("Enter valid phone Number")
       .required("Required"),
     Password: Yup.string()
-      .min(6, "Password must be at least 6 characters")
+      .min(8, "Password must be at least 6 characters")
       .required("Password is required"),
     confirmPassword: Yup.string()
       .oneOf([Yup.ref("Password")], "Password doesn't matched")
@@ -59,144 +56,140 @@ const SignUp = () => {
   return (
     <Grid className="display-center">
       <Paper elevation={20} className="paperStyleSignUP">
-        <Grid align="center">
-          <h2 className="headerMargin" data-testid="titleForSignup">
-            <span className="txtStyle">F</span>
-            <span className="txtStyle2">u</span>
-            <span className="txtStyle">n</span>
-            <span className="txtStyle3">d</span>
-            <span className="txtStyle">o</span>
-            <span className="txtStyle2"> N</span>
-            <span className="txtStyle3">o</span>
-            <span className="txtStyle">t</span>
-            <span className="txtStyle2">e</span>
-          </h2>
-          <h2 className="headerStyle" data-testid="headerForSignup">
-            Sign Up
-          </h2>
-          <Typography
-            className="margin-topBottom"
-            variant="caption"
-            gutterBottom
-            data-testid="capionForSignup"
-          >
-            Please fill this form to create an account !
-          </Typography>
+        <Grid container spacing={2}>
+          <Grid item sm={6} md={6}>
+            <Grid>
+              <h2 data-testid="titleForSignup">
+                <span className="txtStyle">F</span>
+                <span className="txtStyle2">u</span>
+                <span className="txtStyle">n</span>
+                <span className="txtStyle3">d</span>
+                <span className="txtStyle">o</span>
+                <span className="txtStyle2"> N</span>
+                <span className="txtStyle3">o</span>
+                <span className="txtStyle">t</span>
+                <span className="txtStyle2">e</span>
+              </h2>
+              <h2 className="headerStyle" data-testid="headerForSignup">
+                Create your Fundo Account
+              </h2>
+            </Grid>
+            <Formik
+              initialValues={initialValuesSignUp}
+              validationSchema={validationSchemaSignUp}
+              onSubmit={onSubmitSignUP}
+            >
+              {(props) => (
+                <Form data-testid="formForSignUp">
+                  <Grid container spacing={2}>
+                    <Grid item sm={6}>
+                      <Field
+                        as={TextField}
+                        fullWidth
+                        label="First Name"
+                        name="FirstName"
+                        data-testid="FirstNameSignUp"
+                        variant="outlined"
+                        className="bottomMargin"
+                        helperText={<ErrorMessage name="FirstName" />}
+                      />
+                    </Grid>
+                    <Grid item sm={6}>
+                      <Field
+                        as={TextField}
+                        fullWidth
+                        label="Last Name"
+                        name="LastName"
+                        data-testid="LastNameSignUp"
+                        variant="outlined"
+                        className="bottomMargin"
+                        helperText={<ErrorMessage name="LastName" />}
+                      />
+                    </Grid>
+                  </Grid>
+                  <Grid container spacing={0}>
+                    <Grid item sm={12}>
+                      <Field
+                        as={TextField}
+                        fullWidth
+                        label="Email"
+                        name="Email"
+                        data-testid="EmailSignUp"
+                        variant="outlined"
+                        className="bottomMargin"
+                        helperText={<ErrorMessage name="Email" />}
+                      />
+                      <Grid className="setText">
+                        You can use letters, numbers &amp; periods
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                  <Grid container spacing={2}>
+                    <Grid item sm={6}>
+                      <Field
+                        as={TextField}
+                        fullWidth
+                        variant="outlined"
+                        label="Password"
+                        name="Password"
+                        data-testid="PasswordSignUp"
+                        type="password"
+                        helperText={<ErrorMessage name="Password" />}
+                        className="bottomMargin"
+                      />
+                    </Grid>
+                    <Grid item sm={6}>
+                      <Field
+                        as={TextField}
+                        fullWidth
+                        variant="outlined"
+                        label="confirm Password"
+                        name="confirmPassword"
+                        data-testid="confirmPasswordSignUp"
+                        type="password"
+                        helperText={<ErrorMessage name="confirmPassword" />}
+                        className="bottomMargin"
+                      />
+                    </Grid>
+                    <Grid className="setText1">
+                      Use 8 or more characters with a mix of letters, numbers &
+                      symbols
+                    </Grid>
+                  </Grid>
+                  <Grid container spacing={0}>
+                    <Grid item sm={12}>
+                      <FormControlLabel
+                        control={
+                          <Field as={Checkbox} name="termAndCondition" />
+                        }
+                        label="I accept the terms and condition."
+                      />
+                      <FormHelperText>
+                        <ErrorMessage name="termAndCondition" />
+                      </FormHelperText>
+                      <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        fullWidth
+                      >
+                        Sign Up
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </Form>
+              )}
+            </Formik>
+          </Grid>
+          <Grid item sm={6} md={6}>
+            <img
+              className="IMG"
+              src="https://ssl.gstatic.com/accounts/signup/glif/account.svg"
+              alt=""
+            />
+            <p className="imgContain">Quick Notes, To-do List, Reminders</p>
+          </Grid>
         </Grid>
-        <Formik
-          initialValues={initialValuesSignUp}
-          validationSchema={validationSchemaSignUp}
-          onSubmit={onSubmitSignUP}
-        >
-          {(props) => (
-            <Form data-testid="formForSignUp">
-              <Field
-                as={TextField}
-                fullWidth
-                label="First Name"
-                name="FirstName"
-                data-testid="FirstNameSignUp"
-                variant="outlined"
-                className="bottomMargin"
-                helperText={<ErrorMessage name="FirstName" />}
-              />
-              <Field
-                as={TextField}
-                fullWidth
-                label="Last Name"
-                name="LastName"
-                data-testid="LastNameSignUp"
-                variant="outlined"
-                className="bottomMargin"
-                helperText={<ErrorMessage name="LastName" />}
-              />
-              <Field
-                as={TextField}
-                fullWidth
-                label="Email"
-                name="Email"
-                data-testid="EmailSignUp"
-                variant="outlined"
-                className="bottomMargin"
-                helperText={<ErrorMessage name="Email" />}
-              />
-              <FormControl component="fieldset">
-                <FormLabel component="legend">Gender</FormLabel>
-                <Field
-                  as={RadioGroup}
-                  aria-label="gender"
-                  name="Gender"
-                  defaultValue="female"
-                  style={{ display: "initial" }}
-                >
-                  <FormControlLabel
-                    value="female"
-                    control={<Radio />}
-                    label="Female"
-                  />
-                  <FormControlLabel
-                    value="male"
-                    control={<Radio />}
-                    label="Male"
-                  />
-                  <FormControlLabel
-                    value="other"
-                    control={<Radio />}
-                    label="Other"
-                  />
-                </Field>
-              </FormControl>
-              <Field
-                as={TextField}
-                fullWidth
-                label="Phone Number"
-                name="PhoneNumber"
-                data-testid="PhoneNumberSignUp"
-                variant="outlined"
-                className="bottomMargin"
-                helperText={<ErrorMessage name="PhoneNumber" />}
-              />
-              <Field
-                as={TextField}
-                fullWidth
-                variant="outlined"
-                label="Password"
-                name="Password"
-                data-testid="PasswordSignUp"
-                type="password"
-                helperText={<ErrorMessage name="Password" />}
-                className="bottomMargin"
-              />
-
-              <Field
-                as={TextField}
-                fullWidth
-                variant="outlined"
-                label="confirm Password"
-                name="confirmPassword"
-                data-testid="confirmPasswordSignUp"
-                type="password"
-                helperText={<ErrorMessage name="confirmPassword" />}
-                className="bottomMargin"
-              />
-              <FormControlLabel
-                control={<Field as={Checkbox} name="termAndCondition" />}
-                label="I accept the terms and condition."
-              />
-              <FormHelperText>
-                <ErrorMessage name="termAndCondition" />
-              </FormHelperText>
-              <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-                fullWidth
-              >
-                Sign Up
-              </Button>
-            </Form>
-          )}
-        </Formik>
       </Paper>
     </Grid>
   );
